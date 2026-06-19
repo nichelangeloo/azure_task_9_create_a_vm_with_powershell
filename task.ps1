ddbae2b27e9bf2cc0b6f876acbc6c812f9e043ec
Nikita Ivanichenko <importantthings1482@gmail.com>
2026-06-20 01:39:56 +0300

    Solution

--- task.ps1
+++ task.ps1
@@ -1,4 +1,4 @@
-$location = "uksouth"
+$location = "denmarkeast"
 $resourceGroupName = "mate-azure-task-9"
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
@@ -6,8 +6,9 @@ $subnetName = "default"
 $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $publicIpAddressName = "linuxboxpip"
+$dnsPrefix = "matebox-vm"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
+$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub"
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
 $vmSize = "Standard_B1s"
@@ -21,3 +22,25 @@ $nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direct
 New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
 
 # ↓↓↓ Write your code here ↓↓↓
+Write-Host "Creating a virtual network $virtualNetworkName ..."
+$subnet = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix
+New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $subnet
+
+Write-Host "Creating a public IP address $publicIpAddressName ..."
+New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -AllocationMethod Static -Sku Standard -DomainNameLabel $dnsPrefix -Location $location
+
+Write-Host "Creating a SSH key $sshKeyName ..."
+New-AzSshKey -ResourceGroupName $resourceGroupName -Name $sshKeyName -PublicKey $sshKeyPublicKey
+
+Write-Host "Creating a new VM $vmName ..."
+New-AzVM -ResourceGroupName $resourceGroupName `
+         -Name $vmName `
+         -Size $vmSize `
+         -Image $vmImage `
+         -Location $location `
+         -VirtualNetworkName $virtualNetworkName `
+         -SubnetName $subnetName `
+         -PublicIpAddressName $publicIpAddressName `
+         -SecurityGroupName $networkSecurityGroupName `
+         -SshKeyName $sshKeyName `
+         -SecurityType Standard
